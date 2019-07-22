7b3a1f824a2e70f9cba6f9312fd573cd96ecbacc
Austin Teague <ateague@triumphbcap.com>
2019-07-22 12:25:07 -0500

    Render post lists and add posts action and reducer updates

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,16 @@
-export default () => {
-  return 123;
-}
+export default (state = [], action) => {
+  //maintain a list of posts we fetch from the json api
+  // if (action.type === 'FETCH_POSTS') {
+  //   return action.payload;
+  // }
+
+  // return state;
+
+  //This is typically handled in a switch instead of a massive if else tree
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default: 
+      return state;
+  }
+};
